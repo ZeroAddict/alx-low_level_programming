f782f207c3781232f56ea70a683da3230d6def11
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-06-21 07:08:49 -0700

    task 3 x10 alphabet function

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+//gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+
+	for (ten = 97; ten = 122;)
+	{
+		_putchar(ten = 10 * (ten));
+	}
+_putchar('\n');
+}
