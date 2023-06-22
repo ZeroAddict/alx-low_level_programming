ae773c0da317e43417c62c53dcc3dbcb2e59a426
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-06-22 15:43:18 -0700

    task 0-3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,8 @@
-//gcc -Wall -pedantic -Werror -Wextra -std=gnu89 positive_or_negative.c 0-main.c -o 0-main
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
