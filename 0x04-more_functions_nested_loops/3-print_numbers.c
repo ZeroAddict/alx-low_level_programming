04985ba196ae4bdc7d8571f3d179f7bcc005d9d5
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-06-22 22:47:54 -0700

    task 3 faulty comment block excessively  describing function parameter after function definition

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
-*print_numbers - print 0-9
-*@c: number
-*return: 0
+*print_numbers - prints numbers 0-9
 */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (c = '0'; c <= '9'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		_putchar(c);
 	}
+	_putchar('\n');
 }
