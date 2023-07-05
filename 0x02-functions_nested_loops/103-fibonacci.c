094b7d82cab8c7b6dd3437c13fa67ab8af56fbad
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-07-04 20:11:18 -0700

    compilation errors

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 	int i;
 	unsigned long int j, k, next, sum;
 	
-	j = 1
+	j = 1;
 	k = 2;
 	sum = 0;
 	for (i = 1; i <= 33; ++i)
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -24,6 +24,6 @@ int print_sign(int n)
 		return (-1);
 	}
 
-	return 0;
+	return (0);
 
 }
