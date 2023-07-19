57121cd68201e62111395159c637a45b450b0c3b
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-07-19 04:08:59 -0700

    corrected errors concerning float and NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,13 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age > 0)
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("owner: (nil)");
+		printf("owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
