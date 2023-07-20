386bf06fde2b2e0b4627db97ce1223087011d60a
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-07-19 21:50:59 -0700

    changes made to task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,20 +11,21 @@ int main(int argc, char *argv[])
 {
 	int num_of_bytes;
 	char *arr;
-
-	num_of_bytes = atoi(argv[1]);
-
-	if (num_of_bytes < 0)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+	
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	num_of_bytes = atoi(argv[1]);
+
+	if (num_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	unsigned int i;
+	
 	arr = (char *)main;
 	for (i = 0; i < num_of_bytes - 1; i++)
 		{
