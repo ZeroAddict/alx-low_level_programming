60b7b98292547d59ed63754a3ffd6f2d2ca1067a
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-08-07 22:14:03 -0700

    compiled

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,33 +6,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_elf(unsigned char *e_ID);
-void print_magic(unsigned char *e_ID);
-void print_class(unsigned char *e_ID);
-void print_data(unsigned char *e_ID);
-void print_version(unsigned char *e_ID);
-void print_abi(unsigned char *e_ID);
-void print_osabi(unsigned char *e_ID);
-void print_type(unsigned int e_type, unsigned char *e_ID);
-void print_entry(unsigned long int e_entry, unsigned char *e_ID);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
- * @e_ID: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: If the file is not an ELF file - exit code 98.
  */
-void check_elf(unsigned char *e_ID)
+void check_elf(unsigned char *e_ident)
 {
 	int indx;
 
 	for (indx = 0; indx < 4; indx++)
 	{
-		if (e_ID[indx] != 127 &&
-		e_ID[indx] != 'E' &&
-		e_ID[indx] != 'L' &&
-		e_ID[indx] != 'F')
+		if (e_ident[indx] != 127 &&
+		e_ident[indx] != 'E' &&
+		e_ident[indx] != 'L' &&
+		e_ident[indx] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -42,11 +42,11 @@ void check_elf(unsigned char *e_ID)
 
 /**
  * print_magic - Prints the magic numbers of an ELF header.
- * @e_ID: A pointer to an array containing the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: Magic numbers are separated by spaces.
  */
-void print_magic(unsigned char *e_ID)
+void print_magic(unsigned char *e_ident)
 {
 	int indx;
 
@@ -54,7 +54,7 @@ void print_magic(unsigned char *e_ID)
 
 	for (indx = 0; indx < EI_NIDENT; indx++)
 	{
-		printf("%02x", e_ID[indx]);
+		printf("%02x", e_ident[indx]);
 
 		if (indx == EI_NIDENT - 1)
 			printf("\n");
@@ -65,13 +65,13 @@ void print_magic(unsigned char *e_ID)
 
 /**
  * print_class - Prints the class of an ELF header.
- * @e_ID: A pointer to an array containing the ELF class.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_class(unsigned char *e_ID)
+void print_class(unsigned char *e_ident)
 {
 	printf("Class: ");
 
-	switch (e_ID[EI_CLASS])
+	switch (e_ident[EI_CLASS])
 		{
 		case ELFCLASSNONE:
 			printf("none\n");
@@ -83,19 +83,19 @@ void print_class(unsigned char *e_ID)
 			printf("ELF64\n");
 			break;
 		default:
-			printf("<unknown: %x>\n", e_ID[EI_CLASS]);
+			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 		}
 }
 
 /**
  * print_data - Prints the data of an ELF header.
- * @e_ID: A pointer to an array containing the ELF class.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_data(unsigned char *e_ID)
+void print_data(unsigned char *e_ident)
 {
 	printf("Data: ");
 
-	switch (e_ID[EI_DATA])
+	switch (e_ident[EI_DATA])
 		{
 		case ELFDATANONE:
 			printf("none\n");
@@ -107,7 +107,7 @@ void print_data(unsigned char *e_ID)
 			printf("2's complement, big endian\n");
 			break;
 		default:
-			printf("<unknown: %x>\n", e_ID[EI_CLASS]);
+			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 		}
 }
 
@@ -134,7 +134,7 @@ void print_version(unsigned char *e_ident)
  * print_osabi - Prints the OS/ABI of the ELF hdr.
  * @e_ident: A ptr to an array containing the ELF version.
  */
-void print_osabi(unsigned char *e_ID)
+void print_osabi(unsigned char *e_ident)
 {
 	printf("OS/ABI: ");
 
@@ -304,7 +304,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_data(header->e_ident);
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
-	print_abi(header->e_ident;
+	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 
