1c32b6d39e10fb22928447646584a0cad062bbc5
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-08-11 21:57:50 -0700

    made text content empty correctly

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		o = open(filename, O_CREAT);
+		text_content = "";
 	}
 
 	else if (text_content != NULL)
