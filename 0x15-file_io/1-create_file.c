53b1af0b48c3cefc7c8ee17669662b8babc2c3bd
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-08-11 20:17:12 -0700

    treated the empty text_content conditition by creating empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (o == -1 || w_write == -1)
 		return (-1);
-
+	if (text_content == NULL)
+	{
+		o = open(filename, O_CREAT);
+	}
 	close(o);
 
 	return (1);
