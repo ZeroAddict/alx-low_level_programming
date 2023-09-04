5a71fd0e3abb8335ae0a4a00361dadf980fc892d
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-09-04 12:24:05 -0700

    changes made to replace if while loop with for loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,19 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	else if (text_content != NULL)
-	{
-		len = 0;
+	/*
+	*else if (text_content != NULL)
+	*{
+	*	len = 0;
 
-		while (text_content[len])
-		len++;
+	*	while (text_content[len])
+	*	len++;
+	*}
+	*/
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -43,4 +50,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
